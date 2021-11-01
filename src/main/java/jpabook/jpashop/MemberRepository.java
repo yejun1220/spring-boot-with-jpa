2ec7a5b881727f6587723b11ffe8610d9a46c72b
yejun_1220@naver.com <yejun_1220@naver.com>
2021-11-01 19:39:49 +0900

    JPA  + SpringBoot(UPDATE 21.10.31)
    섹션 2 - 엔티티 설계시 주의점까지

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -9,11 +9,16 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MemberRepository {
 
+    // EntityMager 주입
     @PersistenceContext
     private EntityManager em;
 
     public Long save(Member member) {
         em.persist(member);
         return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
     }
 }
