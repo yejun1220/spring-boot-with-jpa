ba7f5a38ca96e838b6ca89285f0a529b4dac9b67
yejun_1220@naver.com <yejun_1220@naver.com>
2021-11-09 23:36:49 +0900

    JPA  + SpringBoot(UPDATE 21.11.09)
    섹션 7 - 홈 화면과 레이아웃까지

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -22,7 +21,15 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m " +
+                "where o.status = :status" +
+                "and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100) // 100번부터
+                .setMaxResults(1000) // 1000번까지
+
+                .getResultList();
+    }
 }
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
@@ -49,10 +51,6 @@ class OrderServiceTest {
 
     }
 
-    @Test
-    public void 주문취소() throws Exception {
-
-    }
 
     @Test
     public void 상품주문_재고수량초과() throws Exception {
@@ -62,10 +60,33 @@ class OrderServiceTest {
         int orderCount = 11; //재고보다 많은 수량
 
         // when
-        orderService.order(member.getId(), item.getId(), orderCount);
-
+        try {
+            orderService.order(member.getId(), item.getId(), orderCount);
+        }
+        catch (NotEnoughStockException e) {
+         return;
+        }
         // then
         fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+
+    @Test
+    public void 주문취소() throws Exception {
+        // given
+        Member member = createMember();
+        Book item = createBook("시골 JPA", 10000, 10);
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        // when
+        orderService.cancelOrder(orderId);
+
+        // then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCEL이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
     }
 
     private Member createMember() {
