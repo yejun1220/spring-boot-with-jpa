e98b81984bb494d32757a3a9abccb1c31980ff74
yejun_1220@naver.com <yejun_1220@naver.com>
2022-07-22 22:30:02 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화 - 간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne
@@ -25,9 +30,33 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
-            order.getMember().getName();
+            order.getMember().getName(); // 프록시가 원래 값을 참조하게 할 뿐 바뀌지 않으므로 Hibernate5Module을 사용은 해야한다.
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = all.stream().map(order -> new SimpleOrderDto(order)).collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    private static class SimpleOrderDto {
+        private final Long orderId;
+        private final String name;
+        private final LocalDateTime orderDate;
+        private final OrderStatus orderStatus;
+        private final Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
     }
 }
