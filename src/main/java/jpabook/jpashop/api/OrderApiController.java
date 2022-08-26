bd42e1ee48c59c70aa8c06769bdf8a2e532d7937
yejun_1220@naver.com <yejun_1220@naver.com>
2022-08-26 23:12:39 +0900

    오타 수정

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -65,7 +65,7 @@ public class OrderApiController {
         return orderQueryRepository.findOrderDtoList();
     }
 
-    @GetMapping("/api/v4/orders")
+    @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
     }
